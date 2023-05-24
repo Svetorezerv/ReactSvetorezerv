ac78193ac84eaa2039bfceca13c1fa0c44305fc1
Рэм Шувалов <shuvalov.rem@mail.ru>
2023-05-24 22:47:27 +0800

    get token explore mobx

--- src/API/AuthorisationService.js
+++ src/API/AuthorisationService.js
@@ -1,7 +1,17 @@
 
 export default class AuthorisationService {
     static async postLogin(username, password) {
-        const response = await fetch(`https://tetreco.com/api/account/{username: '${username}', password: '${password}'}/`);
+        const response = await fetch('https://tetreco.com/api/account/token/', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username,
+                password,
+            }),
+        });
         const result = await response.json();
         return result;
     }
